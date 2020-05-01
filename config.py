4fcf09698b6910e36f036803574cdbdd6b7507f4
Jean <jean.guillet@grenoble-em.com>
2020-05-01 17:37:14 +0200

    Reading env variables from .env

--- config.py
+++ config.py
@@ -1,12 +1,15 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv(override=True)
 
 
 class Config(object):
     DEBUG = False
 
-    OPWM_API_KEY = os.environ["OPWM_API_KEY"]
-    TIMEZONEDB_API_KEY = os.environ["TIMEZONEDB_API_KEY"]
-    AIRQUALITY_API_KEY = os.environ["AIRQUALITY_API_KEY"]
+    OPWM_API_KEY = os.getenv("OPWM_API_KEY")
+    TIMEZONEDB_API_KEY = os.getenv("TIMEZONEDB_API_KEY")
+    AIRQUALITY_API_KEY = os.getenv("AIRQUALITY_API_KEY")
 
 
 class ProductionConfig(Config):
