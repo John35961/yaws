0edcb2c1ed24666ab3536b43ca6828a1bf165ff7
Jean <jean.guillet@grenoble-em.com>
2020-05-11 19:21:25 +0200

    Made chart's hover color stronger

--- app/weather/static/weather/dashboard.js
+++ app/weather/static/weather/dashboard.js
@@ -7,7 +7,8 @@ let weather_temp_forecast_chart = new Chart(ctx, {
         datasets: [{
             label: 'Temperature (°C)',
             data: chartCelTempForecast,
-            borderWidth: 1
+            borderWidth: 1,
+            hoverBackgroundColor: 'rgba(0, 123, 255, 0.5)'
         }, {
             label: 'Feels like (°C)',
             data: chartCelFeelsLikeForecast,
@@ -54,6 +55,7 @@ adaptChartTheme = function() {
     if ($('#darkSwitch').is(':checked')) {
         weather_temp_forecast_chart.data.datasets[0].backgroundColor = 'rgba(222, 113, 25, 0.3)';
         weather_temp_forecast_chart.data.datasets[0].borderColor = 'rgba(222, 113, 25, 1)';
+        weather_temp_forecast_chart.data.datasets[0].hoverBackgroundColor = 'rgba(222, 113, 25, 0.5)';
         weather_temp_forecast_chart.data.datasets[1].backgroundColor = 'rgba(0, 0, 0, 0)';
         weather_temp_forecast_chart.data.datasets[1].borderColor = 'rgba(206, 212, 218)';
         weather_temp_forecast_chart.options.legend.labels.fontColor = 'rgb(172, 170, 158)';
@@ -66,6 +68,7 @@ adaptChartTheme = function() {
     } else {
         weather_temp_forecast_chart.data.datasets[0].backgroundColor = 'rgba(0, 123, 255, 0.3)';
         weather_temp_forecast_chart.data.datasets[0].borderColor = 'rgba(0, 123, 255, 1)';
+        weather_temp_forecast_chart.data.datasets[0].hoverBackgroundColor = 'rgba(0, 123, 255, 0.5)';
         weather_temp_forecast_chart.data.datasets[1].backgroundColor = 'rgba(0, 0, 0, 0)';
         weather_temp_forecast_chart.data.datasets[1].borderColor = 'rgba(34, 34, 34)';
         weather_temp_forecast_chart.options.legend.labels.fontColor = 'rgb(131, 149, 167)';
@@ -73,7 +76,7 @@ adaptChartTheme = function() {
         weather_temp_forecast_chart.options.scales.yAxes[0].ticks.fontColor = 'rgb(131, 149, 167)';
         weather_temp_forecast_chart.options.tooltips.titleFontColor = 'rgb(255, 255, 255)';
         weather_temp_forecast_chart.options.tooltips.bodyFontColor = 'rgb(255, 255, 255)';
-        weather_temp_forecast_chart.options.tooltips.backgroundColor = 'rgba(131, 149, 167, 0.85)'
+        weather_temp_forecast_chart.options.tooltips.backgroundColor = 'rgba(131, 149, 167, 0.85)';
         weather_temp_forecast_chart.update();
     };
 };
