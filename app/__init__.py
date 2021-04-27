01d7c538ac71a4ba2095317dc11c4ad5f072bcdf
Jean <jean.guillet@grenoble-em.com>
2021-04-27 22:25:34 +0200

    Removing create_app function to avoid cyclic import

--- app/__init__.py
+++ app/__init__.py
@@ -1,25 +1,21 @@
 from flask import Flask
 
+application = Flask(__name__,
+                    static_url_path="",
+                    static_folder="static")
 
-def create_app():
-    application = Flask(__name__,
-                        static_url_path="",
-                        static_folder="static")
+# Available environments are Production and Development
+ENV = "Production"
 
-    # Available environments are Production and Development
-    ENV = "Production"
+application.config.from_object(f"config.{ENV}Config")
+application.config['VERSION'] = "1.6.0"
 
-    application.config.from_object(f"config.{ENV}Config")
-    application.config['VERSION'] = "1.6.0"
+from app.weather.routes import weather_blueprint
+from app.map.routes import map_blueprint
+from app.about.routes import about_blueprint
+from app.errors.routes import errors_blueprint
 
-    from app.weather.routes import weather_blueprint
-    import app.map.routes as map_route
-    from app.about.routes import about_blueprint
-    from app.errors.routes import errors_blueprint
-
-    application.register_blueprint(weather_blueprint)
-    application.register_blueprint(map_route.map_blueprint, url_prefix="/map")
-    application.register_blueprint(about_blueprint, url_prefix="/about")
-    application.register_blueprint(errors_blueprint)
-
-    return application
+application.register_blueprint(weather_blueprint)
+application.register_blueprint(map_blueprint, url_prefix="/map")
+application.register_blueprint(about_blueprint, url_prefix="/about")
+application.register_blueprint(errors_blueprint)
