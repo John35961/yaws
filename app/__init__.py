e00e3fb0c7a2794af5f1753581dbc9a7166f6e0c
Jean <jean.guillet@grenoble-em.com>
2021-05-18 21:32:46 +0200

    Resolving circular import in create_app()

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,12 @@ from flask import Flask
 
 
 def create_app():
+
+    from app.weather.routes import weather_blueprint
+    from app.map.routes import map_blueprint
+    from app.about.routes import about_blueprint
+    from app.errors.routes import errors_blueprint
+
     application = Flask(__name__,
                         static_url_path="",
                         static_folder="static")
@@ -11,11 +17,6 @@ def create_app():
 
     application.config.from_object(f"config.{ENV}Config")
     application.config['VERSION'] = "1.6.0"
-
-    from app.weather.routes import weather_blueprint
-    from app.map.routes import map_blueprint
-    from app.about.routes import about_blueprint
-    from app.errors.routes import errors_blueprint
 
     application.register_blueprint(weather_blueprint)
     application.register_blueprint(map_blueprint, url_prefix="/map")
