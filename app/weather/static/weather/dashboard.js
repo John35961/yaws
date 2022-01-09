602185165c0b2ae36d94a1180bb9f05803fb1191
Jean <jean.guillet@grenoble-em.com>
2022-01-09 16:02:06 +0100

    Renaming weather_temp_forecast_chart to weatherTempForecastChart

--- app/weather/static/weather/dashboard.js
+++ app/weather/static/weather/dashboard.js
@@ -1,7 +1,7 @@
 // Instanciating new canvas and new chart
 
-let ctx = document.getElementById('weather_temp_forecast_chart').getContext('2d');
-let weather_temp_forecast_chart = new Chart(ctx, {
+let ctx = document.getElementById('weatherTempForecastChart').getContext('2d');
+let weatherTempForecastChart = new Chart(ctx, {
     type: 'bar',
     data: {
         datasets: [{
@@ -52,6 +52,7 @@ let weather_temp_forecast_chart = new Chart(ctx, {
         maintainAspectRatio: false
     }
 });
+let 
 
 
 // For chart and tooltip, adapting font, background and border colors to selected CSS theme,
@@ -59,35 +60,35 @@ let weather_temp_forecast_chart = new Chart(ctx, {
 
 adaptChartTheme = function() {
     if ($('#darkSwitch').is(':checked')) {
-        weather_temp_forecast_chart.data.datasets[0].backgroundColor = 'rgba(222, 113, 25, 0.3)';
-        weather_temp_forecast_chart.data.datasets[0].borderColor = 'rgba(222, 113, 25, 1)';
-        weather_temp_forecast_chart.data.datasets[0].hoverBackgroundColor = 'rgba(222, 113, 25, 0.5)';
-        weather_temp_forecast_chart.data.datasets[1].backgroundColor = 'rgba(0, 0, 0, 0)';
-        weather_temp_forecast_chart.data.datasets[1].borderColor = 'rgba(206, 212, 218)';
-        weather_temp_forecast_chart.options.legend.labels.fontColor = 'rgb(172, 170, 158)';
-        weather_temp_forecast_chart.options.scales.xAxes[0].ticks.fontColor = 'rgb(172, 170, 158)';
-        weather_temp_forecast_chart.options.scales.yAxes[0].ticks.fontColor = 'rgb(172, 170, 158)';
-        weather_temp_forecast_chart.options.scales.yAxes[0].gridLines.color = 'rgba(72, 71, 67, 0.5)';
-        weather_temp_forecast_chart.options.scales.xAxes[0].gridLines.color = 'rgba(72, 71, 67, 0.5)';
-        weather_temp_forecast_chart.options.tooltips.titleFontColor = 'rgb(246, 244, 230)';
-        weather_temp_forecast_chart.options.tooltips.bodyFontColor = 'rgb(246, 244, 230)';
-        weather_temp_forecast_chart.options.tooltips.backgroundColor = 'rgba(0, 0, 0, 0.85)';
-        weather_temp_forecast_chart.update();
+        weatherTempForecastChart.data.datasets[0].backgroundColor = 'rgba(222, 113, 25, 0.3)';
+        weatherTempForecastChart.data.datasets[0].borderColor = 'rgba(222, 113, 25, 1)';
+        weatherTempForecastChart.data.datasets[0].hoverBackgroundColor = 'rgba(222, 113, 25, 0.5)';
+        weatherTempForecastChart.data.datasets[1].backgroundColor = 'rgba(0, 0, 0, 0)';
+        weatherTempForecastChart.data.datasets[1].borderColor = 'rgba(206, 212, 218)';
+        weatherTempForecastChart.options.legend.labels.fontColor = 'rgb(172, 170, 158)';
+        weatherTempForecastChart.options.scales.xAxes[0].ticks.fontColor = 'rgb(172, 170, 158)';
+        weatherTempForecastChart.options.scales.yAxes[0].ticks.fontColor = 'rgb(172, 170, 158)';
+        weatherTempForecastChart.options.scales.yAxes[0].gridLines.color = 'rgba(72, 71, 67, 0.5)';
+        weatherTempForecastChart.options.scales.xAxes[0].gridLines.color = 'rgba(72, 71, 67, 0.5)';
+        weatherTempForecastChart.options.tooltips.titleFontColor = 'rgb(246, 244, 230)';
+        weatherTempForecastChart.options.tooltips.bodyFontColor = 'rgb(246, 244, 230)';
+        weatherTempForecastChart.options.tooltips.backgroundColor = 'rgba(0, 0, 0, 0.85)';
+        weatherTempForecastChart.update();
     } else {
-        weather_temp_forecast_chart.data.datasets[0].backgroundColor = 'rgba(0, 123, 255, 0.3)';
-        weather_temp_forecast_chart.data.datasets[0].borderColor = 'rgba(0, 123, 255, 1)';
-        weather_temp_forecast_chart.data.datasets[0].hoverBackgroundColor = 'rgba(0, 123, 255, 0.5)';
-        weather_temp_forecast_chart.data.datasets[1].backgroundColor = 'rgba(0, 0, 0, 0)';
-        weather_temp_forecast_chart.data.datasets[1].borderColor = 'rgba(34, 34, 34)';
-        weather_temp_forecast_chart.options.legend.labels.fontColor = 'rgb(131, 149, 167)';
-        weather_temp_forecast_chart.options.scales.xAxes[0].ticks.fontColor = 'rgb(131, 149, 167)';
-        weather_temp_forecast_chart.options.scales.yAxes[0].ticks.fontColor = 'rgb(131, 149, 167)';
-        weather_temp_forecast_chart.options.scales.yAxes[0].gridLines.color = 'rgba(0, 0, 0, 0.1)';
-        weather_temp_forecast_chart.options.scales.xAxes[0].gridLines.color = 'rgba(0, 0, 0, 0.1)';
-        weather_temp_forecast_chart.options.tooltips.titleFontColor = 'rgb(255, 255, 255)';
-        weather_temp_forecast_chart.options.tooltips.bodyFontColor = 'rgb(255, 255, 255)';
-        weather_temp_forecast_chart.options.tooltips.backgroundColor = 'rgba(131, 149, 167, 0.85)';
-        weather_temp_forecast_chart.update();
+        weatherTempForecastChart.data.datasets[0].backgroundColor = 'rgba(0, 123, 255, 0.3)';
+        weatherTempForecastChart.data.datasets[0].borderColor = 'rgba(0, 123, 255, 1)';
+        weatherTempForecastChart.data.datasets[0].hoverBackgroundColor = 'rgba(0, 123, 255, 0.5)';
+        weatherTempForecastChart.data.datasets[1].backgroundColor = 'rgba(0, 0, 0, 0)';
+        weatherTempForecastChart.data.datasets[1].borderColor = 'rgba(34, 34, 34)';
+        weatherTempForecastChart.options.legend.labels.fontColor = 'rgb(131, 149, 167)';
+        weatherTempForecastChart.options.scales.xAxes[0].ticks.fontColor = 'rgb(131, 149, 167)';
+        weatherTempForecastChart.options.scales.yAxes[0].ticks.fontColor = 'rgb(131, 149, 167)';
+        weatherTempForecastChart.options.scales.yAxes[0].gridLines.color = 'rgba(0, 0, 0, 0.1)';
+        weatherTempForecastChart.options.scales.xAxes[0].gridLines.color = 'rgba(0, 0, 0, 0.1)';
+        weatherTempForecastChart.options.tooltips.titleFontColor = 'rgb(255, 255, 255)';
+        weatherTempForecastChart.options.tooltips.bodyFontColor = 'rgb(255, 255, 255)';
+        weatherTempForecastChart.options.tooltips.backgroundColor = 'rgba(131, 149, 167, 0.85)';
+        weatherTempForecastChart.update();
     };
 };
 
@@ -164,11 +165,11 @@ function toCelsius() {
     $('.weather_fah_wind_speed_forecast').css('display', 'none');
     $('.weather_temp_unit').html('°C');
     $('.weather_wind_speed_unit').html('m/s');
-    weather_temp_forecast_chart.data.datasets[0].data = chartCelTempForecast;
-    weather_temp_forecast_chart.data.datasets[1].data = chartCelFeelsLikeForecast;
-    weather_temp_forecast_chart.data.datasets[0].label = 'Temperature (°C)';
-    weather_temp_forecast_chart.data.datasets[1].label = 'Feels like (°C)';
-    weather_temp_forecast_chart.update();
+    weatherTempForecastChart.data.datasets[0].data = chartCelTempForecast;
+    weatherTempForecastChart.data.datasets[1].data = chartCelFeelsLikeForecast;
+    weatherTempForecastChart.data.datasets[0].label = 'Temperature (°C)';
+    weatherTempForecastChart.data.datasets[1].label = 'Feels like (°C)';
+    weatherTempForecastChart.update();
 };
 
 function toFahrenheit() {
@@ -184,9 +185,9 @@ function toFahrenheit() {
     $('.weather_fah_wind_speed_forecast').css('display', 'block');
     $('.weather_temp_unit').html('°F');
     $('.weather_wind_speed_unit').html('mi/h');
-    weather_temp_forecast_chart.data.datasets[0].data = chartFahTempForecast;
-    weather_temp_forecast_chart.data.datasets[1].data = chartFahFeelsLikeForecast;
-    weather_temp_forecast_chart.data.datasets[0].label = 'Temperature (°F)';
-    weather_temp_forecast_chart.data.datasets[1].label = 'Feels like (°F)';
-    weather_temp_forecast_chart.update();
+    weatherTempForecastChart.data.datasets[0].data = chartFahTempForecast;
+    weatherTempForecastChart.data.datasets[1].data = chartFahFeelsLikeForecast;
+    weatherTempForecastChart.data.datasets[0].label = 'Temperature (°F)';
+    weatherTempForecastChart.data.datasets[1].label = 'Feels like (°F)';
+    weatherTempForecastChart.update();
 };
